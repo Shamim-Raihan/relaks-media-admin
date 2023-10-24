31b9bada4930e132040b661052fd68cb02f2b9bb
shakil <shakilkhanhu@gmail.com>
2023-10-24 21:08:45 +0600

    update

--- lib/screens/access_insight/view/access_insight_screen.dart
+++ lib/screens/access_insight/view/access_insight_screen.dart
@@ -1,14 +1,122 @@
 import 'package:flutter/material.dart';
-
-import '../../../utils/app_styles.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AccessInsightScreen extends StatelessWidget {
   const AccessInsightScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Center(
-      child: Text('Access Insight Screen',style: AppStyles().normalTextStyle,),
+    return SizedBox(
+      // color: Colors.yellow,
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Row(
+                // mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Recently Got Access',
+                    style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontSize: 20.sp,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.white),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 15.h,
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: 0.5,
+                child: Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.grey.shade500,
+                        Colors.grey.shade700,
+                      ],
+                      stops: const [0.3, 1.0],
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                    ),
+                  ),
+                ),
+              ),
+              Column(
+                children: [
+                  SizedBox(
+                    height: 30.h,
+                  ),
+                  SizedBox(
+                    // height: MediaQuery.of(context).size.height - 400.h,
+                    // width: MediaQuery.of(context).size.width,
+                    // color: Colors.red,
+                    child: GridView.builder(
+                        shrinkWrap: true,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          mainAxisExtent: 80,
+                          mainAxisSpacing: 0.sp,
+                          crossAxisSpacing: 0.sp,
+                        ),
+                        itemCount: 6,
+                        itemBuilder: (context, index) {
+                          return Column(children: [
+                            Row(
+                              children: [
+                                Image.asset(
+                                  'assets/image/radio.png',
+                                  height: 90.h,
+                                  width: 30.w,
+                                ),
+                                SizedBox(
+                                  width: 2.w,
+                                ),
+                                Column(
+                                  crossAxisAlignment:
+                                      CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    SizedBox(height: 8.h,),
+                                    Text(
+                                      'Relaks Media Give Access',
+                                      maxLines: 2,
+                                      overflow: TextOverflow.ellipsis,
+                                      style: TextStyle(
+                                        fontSize: 10.sp,
+                                          fontFamily: 'Poppins',
+                                          color: Colors.white),
+                                    ),
+                                    SizedBox(
+                                      height: 5.h,
+                                    ),
+                                    Text(
+                                      '3 Hours Ago', overflow: TextOverflow.ellipsis,
+                                      style: TextStyle(
+                                        fontSize: 8.sp,
+                                          fontFamily: 'Poppins',
+                                          color: Colors.grey),
+                                    ),
+                                  ],
+                                )
+                              ],
+                            )
+                          ]);
+                        }),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/screens/audio_content/view/audio_content_screen.dart
+++ lib/screens/audio_content/view/audio_content_screen.dart
@@ -1,14 +1,169 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import '../../../utils/app_styles.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart'; // Make sure to import this
 
 class AudioContentScreen extends StatelessWidget {
-  const AudioContentScreen({super.key});
+  const AudioContentScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-        return Center(
-          child: Text('Audio Content Screen',style: AppStyles().normalTextStyle,),
-        );
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Audio Content Update',
+                    style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontSize: 20.sp,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.white),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 15.h,
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: 0.5,
+                child: Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.grey.shade500,
+                        Colors.grey.shade700,
+                      ],
+                      stops: const [0.3, 1.0],
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                    ),
+                  ),
+                ),
+              ),
+              Column(
+                children: [
+                  SizedBox(
+                    height: 30.h,
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Section 2',
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                        fontSize: 10.sp,
+                        fontFamily: 'Poppins',
+                        color: Colors.white),
+                  ),
+                  ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Color(0xffEA1C24)),
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        'Update Changes',
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                            fontSize: 10.sp,
+                            fontFamily: 'Poppins',
+                            color: Colors.white),
+                      ))
+                ],
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height - 400.h,
+                width: MediaQuery.of(context).size.width,
+                child: ListView.builder(
+                  // scrollDirection: Axis.horizontal,
+                  itemCount: 3, // Displaying 3 containers
+                  itemBuilder: (BuildContext context, int index) {
+                    return Row(
+                      children: [
+                        Container(
+                        height: 300.h,
+                          width: 300.w,
+                          child: Column(
+                            children: [
+                              Container(
+                                height: 100.h,
+                                width: 40.w,
+                                decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.grey),
+                                    borderRadius: BorderRadius.circular(5)
+                                ),
+                                child: Image.asset('assets/image/thumbnail.png',height: 50.h,width: 20.w,),
+                              )
+                            ],
+                          )
+                        )
+                      ],
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
+
+// SizedBox(
+//   child: ListView.builder(
+//     itemCount: 2,
+//       itemBuilder: (context, index)
+//       {
+//     return Column(
+//       children: [
+//         Row(
+//           mainAxisAlignment:MainAxisAlignment.spaceBetween,
+//           children: [
+//             Text(
+//               'Section 2',
+//               maxLines: 2,
+//               overflow: TextOverflow.ellipsis,
+//               style: TextStyle(
+//                   fontSize: 10.sp,
+//                   fontFamily: 'Poppins',
+//                   color: Colors.white),
+//             ),
+//             ElevatedButton(
+//                 style: ButtonStyle(
+//                   backgroundColor: MaterialStateProperty.all(
+//                     Color(0xffEA1C24)
+//                   ),
+//                 ),
+//                 onPressed: () {},
+//
+//                 child: Text(
+//                   'Update Changes',
+//                   maxLines: 2,
+//                   overflow: TextOverflow.ellipsis,
+//                   style: TextStyle(
+//                       fontSize: 10.sp,
+//                       fontFamily: 'Poppins',
+//                       color: Colors.white),
+//                 ))
+//           ],
+//         )
+//       ],
+//     );
+//   }),
+// )
